347a0b79d90709f4740bae929f36d19d02985141
İremsu Koç <iremsuikl@hotmail.com>
2024-01-16 14:27:57 +0300

    blood search service created

---
+++ bloodSearchServiceLogic.js
@@ -0,0 +1,166 @@
+const mysql = require('mysql2/promise');
+const amqp = require('amqplib');
+const fetch = require('isomorphic-fetch');
+
+const getMysqlPool = () => {
+  try {
+    let pool = mysql.createPool({
+      host: 'localhost',
+      user: 'root',
+      password: 'Tokat6035',
+      database: 'blood-donor',
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    });
+
+    console.log('Connection pool created successfully');
+    return pool;
+  } catch (error) {
+    console.error(`Connection pool creation failed: ${error}`);
+    return null;
+  }
+};
+
+
+
+
+const getAllBranches = async () => {
+
+const pool = getMysqlPool();
+
+  try {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query('SELECT city, town , idbranch FROM branch');
+    connection.release();
+
+    return rows;
+  } catch (error) {
+    console.error(`Error getting branch information: ${error}`);
+    throw error;
+  }
+};
+
+
+async function getCoordinates(city, district) {
+    const apiKey = 'c55a20d9e66a41e4b32327807812a923';
+    const encodedCity = encodeURIComponent(city);
+    const encodedDistrict = encodeURIComponent(district);
+    
+    if (!encodedCity || !encodedDistrict) {
+        throw new Error('Invalid city or district');
+    }
+  
+    const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${encodedDistrict},${encodedCity}&key=${apiKey}`;
+  
+    console.log("apiUrl", apiUrl);
+
+
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error(`OpenCage API request failed with status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      if (!data.results || data.results.length === 0 || !data.results[0].geometry) {
+        throw new Error('Invalid or missing data from OpenCage API');
+      }
+  
+      const latitude = data.results[0].geometry.lat;
+      const longitude = data.results[0].geometry.lng;
+      console.log("latitude :", latitude, "longitude : ", longitude);
+      return { latitude, longitude };
+    } catch (error) {
+      console.error('Error retrieving coordinates:', error);
+      throw error;
+    }
+  }
+  
+
+
+
+  function haversine(lat1, lon1, lat2, lon2) {
+    const R = 6371; 
+    const dLat = degToRad(lat2 - lat1);
+    const dLon = degToRad(lon2 - lon1);
+  
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+  
+    return distance;
+  }
+
+
+  function degToRad(deg) {
+    return deg * (Math.PI / 180);
+  }
+
+
+  const searchBlood = async (branchId, bloodType, units) => {
+    const pool = getMysqlPool();
+  
+    if (!pool) {
+      console.error('No MySQL connection pool available.');
+      return null;
+    }
+  
+    try {
+      console.log(`Searching blood for ${units} units in branch ${branchId} with blood type ${bloodType}...`);
+  
+      // Donor query
+      const donorQuery = 'SELECT iddonor, units_of_blood FROM donor WHERE branch_id = ? AND blood_type = ?';
+      const formattedDonorQuery = pool.format(donorQuery, [branchId, bloodType]);
+  
+      console.log('donorQuery:', formattedDonorQuery);
+  
+      const [donorRows] = await pool.execute(formattedDonorQuery);
+      console.log(`Donors with blood type ${bloodType} in branch ${branchId}:`, donorRows);
+  
+      const totalBloodUnits = donorRows.reduce((sum, row) => sum + row.units_of_blood, 0);
+      console.log(`Total blood units available in branch ${branchId} with blood type ${bloodType}: ${totalBloodUnits}`);
+  
+      if (totalBloodUnits >= units) {
+        console.log(`Blood search for ${units} units in branch ${branchId} with blood type ${bloodType}. Total units: ${totalBloodUnits}`);
+  
+        const sortedDonors = donorRows.sort((a, b) => a.units_of_blood - b.units_of_blood);
+  
+        for (const donor of sortedDonors) {
+          const remainingUnits = donor.units_of_blood - units;
+  
+          if (remainingUnits >= 0) {
+            await pool.execute('UPDATE donor SET units_of_blood = ? WHERE iddonor = ?', [remainingUnits, donor.iddonor]);
+            console.log(`Updated donor ${donor.iddonor}. Remaining units: ${remainingUnits}`);
+            break;
+          } else {
+            await pool.execute('UPDATE donor SET units_of_blood = 0 WHERE iddonor = ?', [donor.iddonor]);
+            units -= donor.units_of_blood;
+            console.log(`Updated donor ${donor.iddonor}. Remaining units: 0. ${units} units left.`);
+          }
+        }
+  
+        return 'successful';
+      } else {
+        console.log(`Not enough blood units available in branch ${branchId}. Total units: ${totalBloodUnits}`);
+        return 'rabbitmq';
+      }
+    } catch (error) {
+      console.error('Error searching blood:', error);
+      return null;
+    }
+  };
+  
+  
+  
+
+
+module.exports = {
+    getCoordinates,
+    haversine,
+    getAllBranches,
+    searchBlood,
+};
---
+++ consumeFromRabbitMQ.js
@@ -0,0 +1,159 @@
+const amqp = require('amqplib');
+const processBloodRequest = require('./bloodSearchServiceLogic');
+const cron = require('node-cron');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
+const bloodQueue = 'blood_request_queue';
+const express = require('express');
+
+const app = express();
+const port = 1012;
+
+
+const consumeFromRabbitMQ = async (queueName) => {
+  try {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    console.log(`Connected to RabbitMQ for queue: ${queueName}`);
+
+    await channel.assertQueue(queueName, { durable: true });
+    console.log(`Queue '${queueName}' asserted`);
+
+    channel.prefetch(1);
+
+    console.log(`Waiting for messages on queue: ${queueName}. To exit, press CTRL+C`);
+
+    const processMessage = async (msg) => {
+      const currentTime = new Date().getTime();
+      const messageContent = JSON.parse(msg.content.toString());
+      console.log(`Received message from RabbitMQ on queue ${queueName}:`, messageContent);
+
+      if (messageContent.processed) {
+        console.log('Message already processed. Deleting from the queue.');
+        channel.ack(msg);
+        return;
+      }
+
+      const { selectedCity, selectedTown } = messageContent;
+
+      try {
+        const coordinates = await processBloodRequest.getCoordinates(selectedCity, selectedTown);
+        const branches = await processBloodRequest.getAllBranches();
+
+        if (branches.length === 0) {
+          console.log('No branches found.');
+          channel.ack(msg);
+          return;
+        }
+
+        let bloodSearchResult = null;
+        for (const branch of branches) {
+          try {
+            const branchCoordinates = await processBloodRequest.getCoordinates(branch.city, branch.town);
+            const distance = processBloodRequest.haversine(
+              coordinates.latitude,
+              coordinates.longitude,
+              branchCoordinates.latitude,
+              branchCoordinates.longitude
+            );
+
+            if (distance < 50) {
+              console.log(`Found a match at branch: ${branch.city}, ${branch.town}, Distance: ${distance} km`);
+              bloodSearchResult = await processBloodRequest.searchBlood(branch.idbranch, messageContent.bloodType, messageContent.units);
+              break;
+            } else {
+              console.log(`No match at branch: ${branch.city}, ${branch.town}, Distance: ${distance} km`);
+            }
+          } catch (error) {
+            console.error(`Error processing branch: ${branch.city}, ${branch.town}`, error);
+          }
+        }
+
+        if (bloodSearchResult === 'successful') {
+          console.log(`Blood search successful. Deleting message from the queue ${queueName}.`);
+          messageContent.processed = true;
+          const emailResult = await sendEmail(messageContent.email);
+          console.log(emailResult);
+          channel.ack(msg);
+        } else {
+          if (Number(messageContent.duration) > 0) {
+            console.log(`Blood search unsuccessful. Re-queueing message to retry queue. Remaining duration: ${messageContent.duration}`);
+            
+            messageContent.duration = Math.max(0, Number(messageContent.duration) - 1);
+            
+            channel.sendToQueue(queueName, Buffer.from(JSON.stringify(messageContent)));
+          } else {
+            console.log(`Blood search unsuccessful. Deleting message from the queue ${queueName} as duration is 0.`);
+            messageContent.processed = true;
+            channel.ack(msg);
+          }
+        }
+      } catch (error) {
+        console.error(`Error processing blood request on queue ${queueName}:`, error);
+        channel.ack(msg);
+      }
+    };
+
+    channel.consume(queueName, processMessage, { noAck: false });
+  } catch (error) {
+    console.error(`Error consuming messages from RabbitMQ on queue ${queueName}:`, error);
+  }
+};
+
+cron.schedule('0 1 * * *', async () => {
+  await consumeFromRabbitMQ(bloodQueue);
+  console.log('Job at 1 AM Completed');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+async function clearQueue(queueName) {
+  try {
+    const connection = await amqp.connect('amqp://localhost');
+    console.log('Connected to RabbitMQ');
+
+    const channel = await connection.createChannel();
+    console.log('Channel created');
+
+    await channel.assertQueue(queueName, { durable: true });
+    console.log(`Queue '${queueName}' asserted`);
+
+    const purgeResult = await channel.purgeQueue(queueName);
+    console.log(`Purged ${purgeResult.messageCount} messages from the queue '${queueName}'`);
+
+    await connection.close();
+    console.log('Connection closed');
+  } catch (error) {
+    console.error('Error clearing queue:', error);
+  }
+}
+
+const queueName = 'blood_request_queue';
+//clearQueue(queueName);
+
+
+
+
+const sendEmail = async (to) => {
+  const msg = {
+    to : to,
+    from: {
+      name: 'Health Organization',
+      email: 'iremsuikl@hotmail.com'
+    },
+    subject: "Blood Request",
+    text: "The requested blood type  found",
+  };
+
+  try {
+    await sgMail.send(msg) ;
+    console.log('Email sent successfully.');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
